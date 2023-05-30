6aad5a153bee9880735159b4dca66e504e703f87
JasonBO <JasonBO@JASONBO>
2023-05-30 19:23:43 +1000

    Add project files.

---
+++ EmpowerJB_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerJB_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerJB_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F8D8EAF2-5BF6-4853-9CE7-40FB92E81708")]
+[assembly: AssemblyVersion("1.0.0.0")]
